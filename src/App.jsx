7ab6b6a1b801b2450c05f2cd6b2568a06e784768
Altaf1k1 <altafpathan@gmail.com>
2025-03-16 15:12:39 +0530

    add outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import Header from './componnets/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './componnets/Footer'
 
 function App() {
 
   return (
     <>
      <Header/>
+     <Outlet/>
+     <Footer/>
     </>
   )
 }
