20dad2b96702c28e8f1a86494c58343938566de8
vikas-jangid9 <vikasjangid9571@gmail.com>
2025-03-16 16:40:52 +0530

    vikas-1
    add images amd install packages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Home() {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+
+
   return (
     <div>
-      hii
+      <h1>React Slick Carousel</h1>
+      <Slider {...settings}>
+        <div><h3>Slide 1</h3></div>
+        <div><h3>Slide 2</h3></div>
+        <div><h3>Slide 3</h3></div>
+      </Slider>
     </div>
   )
 }
