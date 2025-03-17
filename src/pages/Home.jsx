134ca7a6eb08a32f062d00e6251e1940b42e914e
Altaf Khan <altafpathan1k1@gmail.com>
2025-03-17 22:47:37 +0530

    add home-banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SlideComp from '../componnets/Slider';
+import '../assets/css/home.css'
 function Home() {
 
  
@@ -8,7 +9,33 @@ function Home() {
 
   return (
     <div>
-      <SlideComp/>
+      <SlideComp/> 
+      <div className='home-banner layout1'>
+
+        <div className='container'>
+          {/* add contant */}
+        </div>
+
+        <div className='container'>
+          {/* add contant */}
+         
+        </div>
+
+        <div className='container'>
+          {/* add contant */}
+          
+        </div>
+  
+
+      </div>
+      <div className='layout2'>
+          <div className='layout2_header'>
+             <p>Shop By Categories</p>
+             <h1>
+              <span>Featured Categories</span>
+              </h1>
+          </div>
+      </div>
     </div>
   )
 }
