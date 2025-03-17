a7de1698f185910fbfebe12a2e7a3aa61975a931
Altaf Khan <altafpathan1k1@gmail.com>
2025-03-17 12:25:21 +0530

    add slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,14 @@
 import React from 'react'
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
+import SlideComp from '../componnets/Slider';
 function Home() {
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-  };
-
+ 
+  
 
 
   return (
     <div>
-      <h1>React Slick Carousel</h1>
-      <Slider {...settings}>
-        <div><h3>Slide 1</h3></div>
-        <div><h3>Slide 2</h3></div>
-        <div><h3>Slide 3</h3></div>
-      </Slider>
+      <SlideComp/>
     </div>
   )
 }
