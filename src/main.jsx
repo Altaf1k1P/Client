b94bc41e46dfb2ba1ec5c0e3b7508f2ac9c67961
Altaf Khan <altafpathan1k1@gmail.com>
2025-03-23 12:53:48 +0530

    contact

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { createBrowserRouter,
   RouterProvider} from 'react-router-dom'
   import Home from './pages/Home.jsx'
-  import contact from './pages/contact.jsx'
+  import Contact from './pages/contact.jsx'
   import Shop from './pages/Shop.jsx'
   import SingleProduct from './pages/SingleProduct.jsx'
 
